41fc7b6879b9f904a74505a694966c23ccafc3e3
thatakin <akinalporhan@gmail.com>
2026-01-08 11:13:06 +1200

    Readjusted the URL of the data source

--- Week 1/korea_fertility.py
+++ Week 1/korea_fertility.py
@@ -1,28 +1,42 @@
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
+from pyfonts import load_font
 import pandas as pd
-from matplotlib import font_manager
 from highlight_text import ax_text
+
 # - Data Files -
-df = pd.read_excel("/Users/akke/Development/Random Projects/52 Charts for 2026/Week 1/Vital Statistics of Korea 2026.xlsx")
+url = "https://github.com/thatakin/52Chartsfor52Weeks/raw/refs/heads/main/Week%201/Vital%20Statistics%20of%20Korea%202026.xlsx"
+df = pd.read_excel(url)
 
 # -- Fonts --
-font = "/Users/akke/Library/Fonts/Commissioner FLAR VOLM.ttf"
-bold_font = FontProperties(fname=font, size=35, weight=900)
-regular_font = FontProperties(fname=font, size=35, weight=400)
-thin_font =FontProperties(fname=font, size=35, weight=100)
+# Using Pyfonts to handle the fonts
+bold = load_font("https://github.com/google/fonts/blob/main/ofl/mukta/Mukta-Bold.ttf?raw=true")
+regular = load_font("https://github.com/google/fonts/blob/main/ofl/mukta/Mukta-Regular.ttf?raw=true")
+thin = load_font("https://github.com/google/fonts/blob/main/ofl/mukta/Mukta-Light.ttf?raw=true")
 
 # -- Plot --
 cm = 1 / 2.54
 fig, ax = plt.subplots(figsize=(22 * cm, 23 * cm), facecolor='none')
+# Adjusting the plot size to accommodate the title
 fig.subplots_adjust(top=0.8)
-ax.plot(df['Year'], df['Fertility Rate'])
+ax.plot(df['Year'], df['Fertility Rate'], color='#0047A0')
 
 # -- Cosmetics --
 ax.grid(visible=False)
 ax.spines[:].set_visible(False)
-ax_text(1950, 7, size=25, font=regular_font, s="<South Korea>'s divindling birth rate", color='#0047A0',
-        highlight_textprops=[{'color':'#CD2E3A', 'size':35, 'font':bold_font}])
+ax_text(1945, 7, size=25, font=regular, s="<South Korea>'s dwindling birth rate", color='#0047A0',
+        highlight_textprops=[{'color':'#CD2E3A', 'size':35, 'font':bold}])
+
+y_max = ax.get_ylim()[1]  # Get the top of y-axis
+x_min = ax.get_xlim()[0]  # Get the left of x-axis
+ax.text(x_min, y_max, "Births\nper woman",
+        va='top', ha='right', fontproperties=thin, size=14)
+
+# Label Font
+font_ticks = regular.copy()
+font_ticks.set_size(14)
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontproperties(font_ticks) 
 
 # -- Arrows --
 ax.annotate("", xytext=(2015, 2), xy=(2018.2, 1),
@@ -32,9 +46,9 @@ ax.annotate("", xytext=(2015, 2), xy=(2018.2, 1),
         connectionstyle='arc3,rad=-0.25',
         shrinkA=0,  
         shrinkB=0))
-ax.text(2015, 2.2, "First time the lowest\nbirthrate in the world", 
-        ha='left', va='bottom',
-        fontproperties=regular_font, weight='500',
-        fontsize=15,
-        )
+ax_text(2015, 2.5, color='black', s="First time the <lowest>\nbirthrate in the world", 
+        ha='right', va='top',
+        font=regular, weight='500',
+        fontsize=15, highlight_textprops=[{'color':'#CD2E3A', 'font':bold}])
+plt.savefig('week1-Korea.svg', transparent=True, dpi=300)
 plt.show()
